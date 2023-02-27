b90e3b3a7e2fb55bae533fd2119307f03cb3104a
melbesnard85 <melbesnard85@gmail.com>
2023-02-28 01:38:00 +0900

    feat: integrated expenses api with db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,21 @@
 import express from 'express'
 import cors from 'cors'
 import { Expenses, PayoutsResuls } from './common/common'
-import { mockExpensesData, mockPayoutsRes } from './common/mockData';
+import { mockPayoutsRes } from './common/mockData';
+import * as firebaseAdmin from "firebase-admin"
+import serviceAccount from './common/bunk-dev-test-firebase-adminsdk-vfeaa-48519ad9f5.json'
+import { getExpenses } from './data-access/status-repository'
 
 const app = express();
 const port = 3000;
+
+const _serviceAccount: object = serviceAccount;
+
+// initializing firebase app
+firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(_serviceAccount),
+    databaseURL: "https://bunk-dev-test-default-rtdb.firebaseio.com"
+})
 
 app.use(express.urlencoded({
     extended: true
@@ -15,12 +26,20 @@ app.get("/", (req, res) => {
     res.send("Hello world!");
 });
 
-app.get("/expenses", (req, res) => {
+app.get("/expenses", async (req, res) => {
     const page = req?.query.page;
     if (page) {
+        const lim_result = req?.query.results;
         // TODO filter data for pagination
-
-        res.send(mockExpensesData);
+        const data: any = await getExpenses(Number(page), Number(lim_result));
+        if (data) {
+            const data_arr = Object.values(data);
+            const rsult = {
+                "recordsTotal": data_arr.length,
+                results: data_arr
+            }
+            res.send(rsult);
+        }
     }
 });
 
