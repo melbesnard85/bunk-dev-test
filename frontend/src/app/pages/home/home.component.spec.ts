e8c939780b951af3b563c45bfc6efa19409ea854
melbesnard85 <melbesnard85@gmail.com>
2023-02-28 10:38:38 +0900

    test: add home test case

--- frontend/src/app/pages/home/home.component.spec.ts
+++ frontend/src/app/pages/home/home.component.spec.ts
@@ -3,12 +3,15 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { firstValueFrom, of } from 'rxjs';
 
 import { HomeComponent } from './home.component';
+import { mockSearchResponse } from '../../mocks/search-response.mock';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  const expenseServiceSpy = jasmine.createSpyObj('getExpenses', ['search']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -27,5 +30,21 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit response when do fetch', (done: DoneFn) => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    const searchBarComponent = fixture.componentInstance;
+
+    expenseServiceSpy.search.and.returnValue(of({ ...mockSearchResponse }));
+    fixture.detectChanges();
+
+    firstValueFrom(searchBarComponent.result$.asObservable()).then((res) => {
+      console.log("res : ", res)
+      expect(res.results).toEqual(mockSearchResponse.results);
+      done();
+    });
+
+    searchBarComponent.onSearch();
   });
 });
